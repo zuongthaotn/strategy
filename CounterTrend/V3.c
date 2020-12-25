1f639662e43f948260c2d145e43e41a55861d4f4
zuongthao <zuongthao@gmail.com>
2020-12-25 08:49:04 +0700

    update V3.c date 25.12

--- CounterTrend/V3.c
+++ CounterTrend/V3.c
@@ -1,7 +1,7 @@
 #include "Strategy/Magnus/fixZorro.h" 
-#define RPM         "CMX", "DPG", "DPM", "GEX", "HAP", "HHS", "PHR", "TCH"
+#define RPM         "ACB", "BFC", "BID", "BWE", "CII", "CMX", "CTG", "DAG", "DPG", "DPM", "DRC", "HAP", "HHS", "HT1", "HVN", "PHR", "PLX", "STB", "TCH", "VRE"
 #define ASSETS      RPM 
-#define N           8
+#define N           20
 var getThreshHold(string ticker);
 
 function run()
@@ -23,21 +23,45 @@ function run()
 var getThreshHold(string ticker){
 	float t;
 	switch(ticker){
+		case "ACB": t = 27.295;
+			break;
+		case "BFC": t = 16.119500000000002;
+			break;
+		case "BID": t = 45.217;
+			break;
+		case "BWE": t = 30.745500000000003;
+			break;
+		case "CII": t = 19.5185;
+			break;
 		case "CMX": t = 15.450000000000001;
 			break;
-		case "DPG": t = 29.252;
+		case "CTG": t = 33.166000000000004;
 			break;
-		case "DPM": t = 18.230999999999998;
+		case "DAG": t = 6.901000000000001;
 			break;
-		case "GEX": t = 21.218000000000004;
+		case "DPG": t = 29.355;
+			break;
+		case "DPM": t = 18.128000000000004;
+			break;
+		case "DRC": t = 21.939;
 			break;
 		case "HAP": t = 6.0358;
 			break;
-		case "HHS": t = 5.0779;
+		case "HHS": t = 5.2015;
 			break;
-		case "PHR": t = 64.375;
+		case "HT1": t = 16.892;
 			break;
-		case "TCH": t = 19.467;
+		case "HVN": t = 26.677;
+			break;
+		case "PHR": t = 63.653999999999996;
+			break;
+		case "PLX": t = 50.985;
+			break;
+		case "STB": t = 16.274;
+			break;
+		case "TCH": t = 19.7245;
+			break;
+		case "VRE": t = 29.458000000000002;
 			break;
 	}
 	return t;
