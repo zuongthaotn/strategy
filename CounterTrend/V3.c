3e95ea85873c4f826a6be9627cdae973e5c3654b
zuongthao <zuongthao@gmail.com>
2021-01-04 10:22:54 +0700

    update V3.c date 04.01

--- CounterTrend/V3.c
+++ CounterTrend/V3.c
@@ -1,7 +1,7 @@
 #include "Strategy/Magnus/fixZorro.h" 
-#define RPM         "BFC", "BWE", "DAG", "DPM", "HAP", "HT1", "PHR", "PVT"
+#define RPM         "CMX", "DAG", "DPG"
 #define ASSETS      RPM 
-#define N           8
+#define N           3
 var getThreshHold(string ticker);
 
 function run()
@@ -23,21 +23,11 @@ function run()
 var getThreshHold(string ticker){
 	float t;
 	switch(ticker){
-		case "BFC": t = 16.119500000000002;
+		case "CMX": t = 16.068;
 			break;
-		case "BWE": t = 30.745500000000003;
+		case "DAG": t = 7.004;
 			break;
-		case "DAG": t = 6.9628;
-			break;
-		case "DPM": t = 18.128000000000004;
-			break;
-		case "HAP": t = 6.0358;
-			break;
-		case "HT1": t = 16.892;
-			break;
-		case "PHR": t = 63.653999999999996;
-			break;
-		case "PVT": t = 12.978;
+		case "DPG": t = 31.003000000000004;
 			break;
 	}
 	return t;
